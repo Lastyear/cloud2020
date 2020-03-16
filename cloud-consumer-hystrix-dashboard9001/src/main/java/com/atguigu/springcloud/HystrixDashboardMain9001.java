c043cf28ee3b84b192a333813db835d1c9a89446
Lastyear <836985507@qq.com>
2020-03-16 18:59:41 +0800

    Hystrix  服务熔断  图形化界面搭建

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -1,21 +1,20 @@
 package com.atguigu.springcloud;
 
-
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * Created by Administrator on 2020/3/16
+ */
 @SpringBootApplication
-@EnableEurekaClient
-@EnableCircuitBreaker
-public class PaymentHystrixMain8001 {
-
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentHystrixMain8001.class, args);
+        SpringApplication.run(HystrixDashboardMain9001.class);
     }
 
     /**
